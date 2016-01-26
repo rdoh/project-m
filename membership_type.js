51895ba448059b40cd62d25c068e3dd9b08fd1f8
Andrew Jones <andrew@andrewjones.id.au>
2016-01-27 10:51:02 +1100

    fix syntax so test executes all assertions and exits cleanly

--- membership_type.js
+++ membership_type.js
@@ -1,15 +1,17 @@
 casper.test.begin('The heading exists', 2, function suite(test){
   casper.start('https://project-m-staging.herokuapp.com',function(){
-    test.assertExists('Membership Type');
-    })});
+      test.assertExists('Membership Type');
+  });
 
-  casper.then(function() {
-        this.evaluate(function(term) {
-            document.querySelector('input[name=isEnrolled][value=yes]').setAttribute('checked', true);
-            document.querySelector('input[name=residentialStatus][value=citizen]').setAttribute('checked', true);
-            document.querySelector('input[name=isMemberOfOtherParty][value=yesOp]').setAtttribute('checked',true);
-        });
-     
-        this.test.assertExists('You are entitled to a Supporter Membership')}
+    casper.then(function() {
+        document.querySelector('input[name=isEnrolled][value=yes]').setAttribute('checked', true);
+        document.querySelector('input[name=residentialStatus][value=citizen]').setAttribute('checked', true);
+        document.querySelector('input[name=isMemberOfOtherParty][value=yesOp]').setAtttribute('checked',true);
+        
+        test.assertExists('You are entitled to a Supporter Membership')}
   );
 
+    casper.run(function() {
+        test.done();
+    })
+});
